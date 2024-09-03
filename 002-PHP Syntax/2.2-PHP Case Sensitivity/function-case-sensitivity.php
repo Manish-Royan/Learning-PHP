a3c7fefb5e3bab22ada4c4a557f43ab5c890b2c6
Manish Royan <manishroyanofficial@outlook.com>
2024-09-04 00:39:32 +0545

    PHP Syntax

---
+++ 002-PHP Syntax/2.2-PHP Case Sensitivity/function-case-sensitivity.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>  
+<html>  
+    <body>  
+        <?php  
+            echo "Hello world using echo </br>";//output: Hello world using echo
+            ECHO "Hello world using ECHO </br>";//output: Hello world using ECHO 
+            EcHo "Hello world using EcHo </br>"; //output: Hello world using EcHo
+        ?>  
+    </body>  
+</html>  
