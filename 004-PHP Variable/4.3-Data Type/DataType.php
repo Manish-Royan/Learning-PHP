709128a109cb622398e2936ce87cd6fa77d687eb
Manish Royan <manishroyanofficial@outlook.com>
2024-09-06 00:51:31 +0545

    PHP DataType

---
+++ 004-PHP Variable/4.3-Data Type/DataType.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //Data Types: PHP variables are loosely typed, meaning you do not need to declare the data type when defining a variable. Types can change dynamically based on the assigned value.
+    
+    $number = 42;          // integer
+    $name = "Alice";       // string
+    $isReady = true;       // boolean
+    $price = 19.99;        // float
+
+    ?>
+</body>
+</html>
