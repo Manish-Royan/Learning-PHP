f6b9f912eff9d5a961568a4c9905dbe0f20c3b14
Manish Royan <manishroyanofficial@outlook.com>
2024-09-07 03:26:40 +0545

    PHP ArithmeticOperation

---
+++ ArithmeticOperation/comparison.php
@@ -0,0 +1,7 @@
+<?php
+    $a = 10;
+    $b = 5;
+    $c = $a <> $b; // is same to != not equal to
+    $d = "Sax";
+    $c !== $d; // there should their value and dataType shouldn't be equal so it will give 'true'
+?>
