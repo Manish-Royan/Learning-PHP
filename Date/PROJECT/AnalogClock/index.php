ee5430564b392f91b44f39560ec64d190303f44c
Manish Royan <manishroyanofficial@outlook.com>
2024-09-12 03:44:40 +0545

    Analog Clock Api

---
+++ Date/PROJECT/AnalogClock/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron text-center">
+            <h1>Analog Clock</h1>
+            <iframe src="https://free.timeanddate.com/clock/i9icwysl/n117/szw210/szh210/cf100/hnce1ead6" frameborder="0" width="210" height="210"></iframe>
+            
+        </div>
+    </div>
+</body>
+</html>
