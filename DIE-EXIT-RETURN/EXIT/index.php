f9cc23235b70d58cdd105b14735ba7605c018242
Manish Royan <manishroyanofficial@outlook.com>
2024-09-13 03:46:47 +0545

    PHP EXIT

---
+++ DIE-EXIT-RETURN/EXIT/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 10;
+$b = 5;
+
+echo $a+$b ."<br/>"; //output: 15
+
+
+echo $a-$b ."<br/>"; //output: 5
+
+exit; //the exection of program ends here
+
+echo $a*$b ."<br/>"; //won't execute
+echo $a/$b ."<br/>"; //won't execute
+
+?>
